670d33594d8044e335b446eabede358565dd7717
abisheksde <abisheksde@gmail.com>
2024-05-31 10:20:00 +0530

    Complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React , { useState } from 'react';
 import Navbar from "./components/Navbar";
+import Axios from 'axios';
 
 function App() {
+  // let options = {
+  //   headers: {accept:'application/json'}
+  // }
+  // Axios.get('https://demo-blog.mashupstack.com/api/posts', options).then(request=> {
+  //   console.log(request.data);
+  // }).catch(error=>{
+  //   console.log("ERROR");
+  // })
+
+  var data = {
+    
+    title: "Nothing to Worry",
+    content:"lorem Ispum Ultimate",
+  };
+  var options = {
+      headers: {'Accept':'application/json'},
+  }
+  Axios.post('https://demo-blog.mashupstack.com/api/posts',data,options).then(response=>{
+      // statements to execute on success response from api endpoint
+      console.log(response.data)
+  }).catch(error=>{
+      // statements to execute on error response from api endpoint
+      console.log(error)
+  })
   return(
   <div>
     <Navbar/>
